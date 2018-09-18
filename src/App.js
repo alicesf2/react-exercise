5b169f792e85589ecc0986c4175f4eb5673470da
Alice Fang <alicesf2@illinois.edu>
2018-09-17 22:18:42 -0500

    finished part 4 kinda

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import { Counter } from './components'
 import './styles/app.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '0', initialCount: '0' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({ initalCount: this.state.value })
+    alert('Initial count was changed to ' + this.state.value + '.')
+  }
+
   render() {
     return (
       <div className="app">
@@ -12,7 +29,18 @@ class App extends Component {
           shouldDisplayImage={true}
           items={['apple', 'banana', 'clementine', 'dragonfruit', 'bl']}
         />
-        <Counter />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Count:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <Counter count={parseInt(this.state.initialCount)} />
       </div>
     )
   }
