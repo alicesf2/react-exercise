74f86f19445a1521fd09544216aff6561bbf8ce0
Alice Fang <alicesf2@illinois.edu>
2018-09-17 23:22:06 -0500

    added some css styling

--- src/components/Counter.component.js
+++ src/components/Counter.component.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react'
+import '../styles/counter.css'
 
 class Counter extends Component {
   constructor(props) {
     super(props)
     this.state = { count: 0 }
-
-    //bind this to function
     this.incrementCount = this.incrementCount.bind(this)
     this.decrementCount = this.decrementCount.bind(this)
   }
 
-  //increment count function
   incrementCount() {
     this.setState(state => ({
       count: (this.state.count += 1)
     }))
   }
 
-  //decrement count function
   decrementCount() {
     this.setState(state => ({
       count: (this.state.count -= 1)
@@ -28,8 +25,12 @@ class Counter extends Component {
     return (
       <div className="counter">
         <div>Current Count: {this.state.count}</div>
-        <button onClick={this.incrementCount}>Increment Count</button>
-        <button onClick={this.decrementCount}>Decrement Count</button>
+        <button className="green-button" onClick={this.incrementCount}>
+          Increment Count
+        </button>
+        <button className="red-button" onClick={this.decrementCount}>
+          Decrement Count
+        </button>
       </div>
     )
   }
---
+++ src/components/InitialCountForm.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+
+class InitialCountForm extends App {
+  //// TODO: move input and submit button here
+}
